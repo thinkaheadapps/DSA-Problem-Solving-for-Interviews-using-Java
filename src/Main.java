6d180b5b5a51d95846f726a52c3284fbe69388f5
Divya Rai <thinkaheadapps@gmail.com>
2024-03-14 22:11:09 +0530

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,13 @@ public class Main {
         System.out.println("Git");
     }
 }
+
+/*
+echo "# Java-DSA-Course" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/thinkaheadapps/Java-DSA-Course.git
+git push -u origin main
+ */
